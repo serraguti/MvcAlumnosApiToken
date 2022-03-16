786fc8c2d8c8ab54df4b0b7a1cc367f5dee75361
serraguti <pacoserranox@gmail.com>
2022-03-16 13:06:58 +0100

    Add project files.

---
+++ MvcAlumnosApiToken/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAlumnosApiToken.Models
+{
+    public class Alumno: TableEntity
+    {
+        public string IdAlumno { get; set; }
+        public string Curso { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Nota { get; set; }
+    }
+}
---
+++ MvcAlumnosApiToken/Services/ServiceApiToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MvcAlumnosApiToken.Services
+{
+    public class ServiceApiToken
+    {
+        private string UrlApi;
+
+        public ServiceApiToken(string urlapi)
+        {
+            this.UrlApi = urlapi;
+        }
+
+        public async Task<string> GetTokenAsync(string curso)
+        {
+            string request = "/api/alumnoskeys/token/" + curso;
+            using (WebClient client = new WebClient())
+            {
+                client.Headers["content-type"] = "application/json";
+
+            }
+        }
+    }
+}
